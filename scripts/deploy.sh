89233560de77ad27b1fe62a5f4d26025cf1c298c
5STARJeongHee <qhxmaoflr@gmail.com>
2021-07-29 00:01:52 +0900

    tomcat 컨테이너에서 서버 세팅의 base 폴더 명과 일치

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ REPOSITORY=/home/ubuntu/app/step2
 PROJECT_NAME=cogather-webservice
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.war $REPOSITORY/zip/docker/webapps
+mv $REPOSITORY/zip/*.war $REPOSITORY/zip/docker/webapps/cogather.war
+# cp $REPOSITORY/zip/cogather.war $REPOSITORY/zip/docker/webapps/cogather.war
 
 cd $REPOSITORY/zip/docker && sudo docker-compose up -d 
