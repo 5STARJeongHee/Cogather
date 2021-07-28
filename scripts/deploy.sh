c6a5d9c6270f9cc5eb2f5b8d018157f4febe6643
5STARJeongHee <qhxmaoflr@gmail.com>
2021-07-28 17:11:51 +0900

    배포수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ REPOSITORY=/home/ubuntu/app/step2
 PROJECT_NAME=cogather-webservice
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/*.war $REPOSITORY/docker/webapps
+cp $REPOSITORY/zip/*.war $REPOSITORY/zip/docker/webapps
 
-cd $REPOSITORY/docker && sudo docker-compose up -d 
+cd $REPOSITORY/zip/docker && sudo docker-compose up -d 
