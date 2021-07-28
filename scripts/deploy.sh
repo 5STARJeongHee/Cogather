a21ab18cab7cc8d936ecbdb981ef8bc90160ecab
5STARJeongHee <qhxmaoflr@gmail.com>
2021-07-28 13:58:47 +0900

    배포 설정 파일 경로 오류 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/app/step2
-DESTINATION=/home/ubuntu/Cogather/docker/webapps
+DESTINATION=/home/ubuntu/Cogather/docker
 PROJECT_NAME=cogather-webservice
 
 echo "> Build 파일 복사"
-cp $REPOSITORY/zip/**/*.war $DESTINATION/
+cp $REPOSITORY/zip/**/*.war $DESTINATION/webapps
 
+cd $DESTINATION && compose up -d 
