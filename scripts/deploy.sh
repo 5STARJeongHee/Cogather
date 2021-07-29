65afb9472b4a323232b4a3c04a947474ac51cb25
5STARJeongHee <qhxmaoflr@gmail.com>
2021-07-29 13:35:11 +0900

    톰캣 컨테이너만 제거 후 다시 compose-up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,8 @@ REPOSITORY=/home/ubuntu/app/step2
 PROJECT_NAME=cogather-webservice
 
 echo "> Build 파일 복사"
+sudo docker stop cogather
+sudo docker rm cogather 
 rm -rf $REPOSITORY/zip/docker/webapps/*.war
 mv $REPOSITORY/zip/*.war $REPOSITORY/zip/docker/webapps/cogather.war
 # cp $REPOSITORY/zip/cogather.war $REPOSITORY/zip/docker/webapps/cogather.war
